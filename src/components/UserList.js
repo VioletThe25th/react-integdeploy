bd26f00c2f39c882ae72c12ecbd82a3db1d00e07
VioletThe25th <bilgerjeremy5705@gmail.com>
2024-10-08 16:22:18 +0200

    update UserList

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,13 +1,19 @@
-import React from "react";
+// src/components/UserList.js
+
+import React from 'react';
 
 const UserList = ({ users }) => {
-    return (
-        <ul>
-            {users.map((user, index) => {
-                <li key={index}>{`${user.firstName} ${user.lastName}, ${user.city}, ${user.postalCode}`}</li>
-            })}
-        </ul>
-    );
+  return (
+    <ul>
+      {users.map((user, index) => {
+        return (
+          <li key={index}>
+            {`${user.firstName} ${user.lastName}, ${user.city}, ${user.postalCode}`}
+          </li>
+        );
+      })}
+    </ul>
+  );
 };
 
 export default UserList;
